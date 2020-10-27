b15574de43db80a1db238f33cd504ea70f146582
tarakdavid <david.tarakjian@mail.mcgill.ca>
2020-10-27 17:06:52 -0400

    corrected log reg model label classification

--- src/algo/dummy_model.py
+++ src/algo/dummy_model.py
@@ -49,33 +49,46 @@ class Stock_model(BaseEstimator, TransformerMixin):
         print("X")
         print(X)
         data = self._data_fetcher(X, last=True)
+        # result = []
+        # for i in range(0, len(data)):
+        #     if data.iloc[i, 3] > data.iloc[i - 1, 3]:
+        #         result.append(0)
+        #     else:
+        #         result.append(1)
+        # data['buy'] = result
+        print("data")
+        print(data)
+        print(data.columns)
+        df_features = create_features(data)
+        print("df_Feature")
+        print(df_features)
+        #acc_f = clf.score(x_testf, y_testf)
+        #prec_f = precision_score(y_testf, yf_pred)
+        #bal_f = balanced_accuracy_score(y_testf, yf_pred)
+        df_features, Y = create_X_Y(df_features)
+        predictions = self.lr.predict(df_features)
         result = []
         for i in range(0, len(data)):
-            if data.iloc[i, 3] > data.iloc[i - 1, 3]:
+            if data.iloc[i, 0] > predictions[-1]:
                 result.append(0)
             else:
                 result.append(1)
         data['buy'] = result
-        print("data")
+        print("data again")
         print(data)
-        df_features = create_features(data)
-        print("df_Feature")
-        print(df_features)
+        print("predictions")
+        print(predictions)
+        predictions.shape
         Xforest = data[['high', 'open', 'low', 'close']]
         Yforest = data['buy']
         x_trainf, x_testf, y_trainf, y_testf = train_test_split(Xforest, Yforest, test_size=.20)
         log = LogisticRegression(random_state=0)
         log.fit(x_trainf, y_trainf)
         ylog_pred = log.predict(x_testf)
-        #acc_f = clf.score(x_testf, y_testf)
-        #prec_f = precision_score(y_testf, yf_pred)
-        #bal_f = balanced_accuracy_score(y_testf, yf_pred)
         bal_log = balanced_accuracy_score(y_testf, ylog_pred) * 100
-        df_features, Y = create_X_Y(df_features)
         close = data['open']
-        predictions = self.lr.predict(df_features)
         predictions2 = log.predict(Xforest)
-        predictions2 = np.where(predictions == 1, 'Sell', 'Buy')
+        predictions2 = np.where(predictions2 == 1, 'Buy', 'Sell')
         predictions2 = predictions2.flatten()[-1]
         #print("predictions2")
         #print(predictions2)
