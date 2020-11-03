efced3c6316c0a92f5607099bcf2e0c3719f6fe6
tarakdavid <david.tarakjian@mail.mcgill.ca>
2020-11-03 12:52:43 -0500

    upgraded log model

--- src/IO/get_data_from_yahoo.py
+++ src/IO/get_data_from_yahoo.py
@@ -6,6 +6,6 @@ from yahoo_fin import stock_info as si
 def get_last_stock_price(ticker, last=False):
     if last:
         now = datetime.now()
-        start_date = now - timedelta(days=30)
+        start_date = now - timedelta(days=7201)
         return si.get_data(ticker, start_date)
     return si.get_data(ticker)
--- src/algo/dummy_model.py
+++ src/algo/dummy_model.py
@@ -8,6 +8,7 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import balanced_accuracy_score
 from sklearn.model_selection import train_test_split
 import numpy as np
+# from pandas_datareader import data
 
 
 def create_features(df_stock, nlags=10):
@@ -49,6 +50,9 @@ class Stock_model(BaseEstimator, TransformerMixin):
         print("X")
         print(X)
         data = self._data_fetcher(X, last=True)
+        # data2 = data.DataReader(X,data_source='yahoo',start='2000-01-01',end='2020-10-14')
+        # print("data2")
+        # print(data2)
         # result = []
         # for i in range(0, len(data)):
         #     if data.iloc[i, 3] > data.iloc[i - 1, 3]:
@@ -68,8 +72,13 @@ class Stock_model(BaseEstimator, TransformerMixin):
         df_features, Y = create_X_Y(df_features)
         predictions = self.lr.predict(df_features)
         result = []
+        # for i in range(0, len(data)):
+        #     if data.iloc[i, 0] > predictions[-1]:
+        #         result.append(0)
+        #     else:
+        #         result.append(1)
         for i in range(0, len(data)):
-            if data.iloc[i, 0] > predictions[-1]:
+            if data.iloc[i, 3] > data.iloc[i - 1, 3]:
                 result.append(0)
             else:
                 result.append(1)
@@ -78,15 +87,23 @@ class Stock_model(BaseEstimator, TransformerMixin):
         print(data)
         print("predictions")
         print(predictions)
-        predictions.shape
-        Xforest = data[['high', 'open', 'low', 'close']]
+        Xforest = data[['high', 'open', 'low', 'close', 'adjclose']]
         Yforest = data['buy']
         x_trainf, x_testf, y_trainf, y_testf = train_test_split(Xforest, Yforest, test_size=.20)
-        log = LogisticRegression(random_state=0)
+        log = LogisticRegression(C=5, class_weight=None, dual=False, fit_intercept=True,
+                   intercept_scaling=1, l1_ratio=None, max_iter=100,
+                   multi_class='auto', n_jobs=None, penalty='l2',
+                   random_state=0, solver='lbfgs', tol=0.0001, verbose=0,
+                   warm_start=False)
         log.fit(x_trainf, y_trainf)
         ylog_pred = log.predict(x_testf)
         bal_log = balanced_accuracy_score(y_testf, ylog_pred) * 100
-        close = data['open']
+        print("balanced accuracy")
+        print(bal_log)
+        print("y_testf and ylog_pred")
+        print(y_testf)
+        print(ylog_pred)
+        close = data['close']
         predictions2 = log.predict(Xforest)
         predictions2 = np.where(predictions2 == 1, 'Buy', 'Sell')
         predictions2 = predictions2.flatten()[-1]
